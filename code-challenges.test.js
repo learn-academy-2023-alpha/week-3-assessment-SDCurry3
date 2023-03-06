2404f8c82a7ec7ee57e22e7f1caee07dfd6e8459
SDCurry3 <sncy23@gmail.com>
2023-03-06 00:03:52 -0800

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,18 +14,55 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const fibLength1 = 6
+describe("highschoolFlashbacks", () => {
+  const fibLength1 = 6
+  const fibLength2 = 10
+  it("does stupid math that stinks because numbers make my head hurt Q_Q", () => {
+    expect(highschoolFlashbacks(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+    expect(highschoolFlashbacks(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+  })
+});
+
+// *****Due to my numerically deficient nature I didn't create the test until after getting the code working, sorry.*****
+
+const fibLength1 = 6;
 // Expected output: [1, 1, 2, 3, 5, 8]
 
-const fibLength2 = 10
+
+const fibLength2 = 10;
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
 // b) Create the function that makes the test pass.
+
+// input: a number that is greater than 2.
+// output: an array of the same length as input containing the numbers of the Fibonacci sequence.
+
+// Pseudocode: create a function containing a for loop and a small predefined array of [1, 1], that will take an input number and continually add the first index of the array by the zero-th, (*****Can I just say how annoying 'zero-th' is to pronounce? whoever made that a thing needs a stern talking-to, methinks...*****) the number of times designated by the input. each time the operation is performed, the array should be modified by the resulting addition. This way, the array can store the math until the for loop completes and the final array is returned.
+
+// *****Dear god I hope I pseudo-ed this correctly. Math-y thoughts are REALLY hard for me to express properly, and even getting this code to run was more difficult than it should've been. 'Muh brain dumb.*****
+
+const highschoolFlashbacks = (eternalPain) => {
+  const allIsDarkness = [1, 1];
+  for (let endMySuffering = 2; endMySuffering < eternalPain; endMySuffering++) {
+    allIsDarkness[endMySuffering] = allIsDarkness[endMySuffering - 2] + allIsDarkness[endMySuffering - 1];}
+  return allIsDarkness
+};
 
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
 
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("lofiGirlApproved", () => {
+  it("tracks how long you've been study/relaxing", () => {
+    expect(lofiGirlApproved(studyMinutesWeek1)).toEqual([15, 15, 20, 30, 30, 60, 90])
+    expect(lofiGirlApproved(studyMinutesWeek2)).toEqual([10, 15, 20, 45, 60, 65, 100])
+  })
+});
+
+// [1st error: ReferenceError: lofiGirlApproved is not defined **initial run**]
+// [2nd error: TypeError: lofiGirlApproved(...).toEqual is not a function **Perplexingly improper Paranthetical Placement**]
+// [3rd error: None! this one was blissfully simple. Go Go Gadget built-in methods!]
 
 const studyMinutesWeek1 = {
   sunday: 90,
@@ -37,7 +74,6 @@ const studyMinutesWeek1 = {
   saturday: 60
 }
 // Expected output: [15, 15, 20, 30, 30, 60, 90]
-
 const studyMinutesWeek2 = {
   sunday: 100,
   monday: 10,
@@ -47,21 +83,76 @@ const studyMinutesWeek2 = {
   friday: 15,
   saturday: 65
 }
+
 // Expected output: [10, 15, 20, 45, 60, 65, 100]
 
 // b) Create the function that makes the test pass.
+
+// input: an object
+// output: an array of the values sorted from least to greatest.
+
+//pseudocode: create a function with one input that takes in an object, runs Object.values to extract the values into a subarray, and then returns an array sorted by length with [.sort((a, b) => a - b)], which in this configuration sorts from least to greatest.
+
+const lofiGirlApproved = (input) => {
+  let entries = Object.values(input)
+  return entries.sort((a, b) => a - b)
+}
+
+// *****Go go gadget, Built-in methods! this one was pretty easy to get running once I figured out how Object.values works, thanks for that hint! although the capital O in object threw me off for a minute. TIL all built-in classes start with a capital letter.*****
+
+
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
 
+describe("piggyBanker", () => {
+  it("shows you how broke you are, in real time!", () => {
+    expect(piggyBanker(accountTransactions1)).toEqual([100, 83, 60, 51])
+    expect(piggyBanker(accountTransactions2)).toEqual([250, 161, 261, 165])
+    expect(piggyBanker(accountTransactions3)).toEqual([])
+  })
+})
+
+//[1st error:  Missing second argument. It must be a callback function. Perhaps you want to use `test.todo` for a test placeholder.
+    //   88 |
+    //   89 | describe("piggyBanker", () => {
+    // > 90 |   it("shows you how broke you are, in real time!"), () => {             ***** oops. more improper paranthetical placement.*****]
+
+// [2nd error:   expect(received).toEqual(expected) // deep equality
+
+    // - Expected  - 3
+    // + Received  + 3
+
+    //   Array [250,
+    // -   -89,
+    // -   100,
+    // -   -96,
+    // +   161,
+    // +   261,
+    // +   165, ]               ***I'm pretty annoyed at how long it took me to notice that I fed the test the wrong expected array...***
+
 const accountTransactions1 = [100, -17, -23, -9]
 // Expected output: [100, 83, 60, 51]
-
 const accountTransactions2 = [250, -89, 100, -96]
 // Expected output: [250, 161, 261, 165]
-
 const accountTransactions3 = []
 // Expected output: []
 
 // b) Create the function that makes the test pass.
+
+// input: an array
+// output: an array of the accumulating sum, unless the array is empty because then you get NOTHING!
+
+//pseudocode: create a function with a single input and a 'container' variable initially set to 0, that will iterate over a given array, adding('math' adding, not just sticking it into the array like with .push) each value within to the container variable. can do this with .map, configured to increase the container value by the number inside the array
+
+const piggyBanker = (input) => {
+  let howBrokeYouAre = 0
+  return input.map((input) => {
+    howBrokeYouAre += input
+    return howBrokeYouAre;
+  })
+};
+
+// *****Booooo more math! Ok fine this one wasn't as bad, I think I might actually be starting to get the hang of .map, yay! 
+//    All this does is pretty much what I had down in pseudocode, we set an initial value of 0, then start tracking the result of adding the array by whatever number is currently in howBrokeYouAre. since adding a negative number == subtracting by that much, we won't see negative values in the array with the given inputs.*****
--- interview-questions.md
+++ interview-questions.md
@@ -8,48 +8,54 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer: Javascript Syntax eXchange? I forget what the acronym means but it's what allows javascript to play with HTML's toys.
 
-Researched answer:
-
+Researched answer: Dang, I was close. The acronym is Javascript Syntax eXtension, or just Javascript XML. An interesting Syntactical difference would be the need to dance around Javascript's reserved keywords, like how "for" in HTMLspeak would be "HTMLfor".
+***something about the phrase "syntactic sugar" really irks me...***
 1. What is yarn? What file(s) are modified when you run the command yarn in your terminal?
 
-Your answer:
+Your answer: A package manager, similar to NPM, that we use to install/run React and Jest.
 
-Researched answer:
+***yarn is a long continuous length of interlocked fibres, used in sewing, crocheting, knitting, weaving, embroidery, ropemaking, and the production of textiles. Yarn is the package manager. gotta watch that Capitalization!***
+
+Researched answer: running [yarn] in my terminal gives me the yarn.lock file and a node_modules folder.
 
 3. What is a React component?
 
-Your answer:
+Your answer: a react component is a bit of code in its' own file, made to be reuseable in different applications. say you have a pretty box or CSS animation that you like, If you create it as a react component you can use it wherever and whenever you like!
+**I'd say you could probably make a decent side hustle out of creating quality component libraries, but all these programmers just wanna "share" their code for "free" because "an open web is a better web". Dang cyber-hippies!**
 
-Researched answer:
+Researched answer: What my prior answer says, but way smarter and more official sounding.
 
 4. What is the difference between state values and props in React?
 
-Your answer:
+Your answer: state values can change, props are static.
 
-Researched answer:
+Researched answer: Ok so apparently this topic is way more complex and technical than I realized, and there are a ton of fiddly little differences between the two, but in general a component's prop(ertie)s are much more static than its state values.
 
 5. What is the DOM?
 
-Your answer:
-
-Researched answer:
-
+Your answer: Blind guess on the acronym; Document Object Manager? Regardless of what it's called, The DOM is like a transit hub, handling the import/exporting of components inside the react application.
+***...heh...***
+Researched answer: On the right track, but not quite deep enough. the Documents Object Model isn't quite like a Transit Hub, but more like a deployment strategy. It's what handles changes to the UI on a page, updating based on user input. React brings a Virtual DOM, greatly improving this method by internally breaking the app into a 'tree' of components, only updating whatever is necessary instead of the whole app whenever an individual element is manipulated.
+ **Hey, I was close on that acronym too!**
 6. STRETCH: Which is the difference between a div tag and a span tag?
 
-Your answer:
+Your answer: Spans are smaller! IIRC a <div> tag will start a whole new page element, which may create blocking issues (stage blocking, not programmer blocking) with other elements on a page. spans are only as big as the thing inside it, so won't take up any extra horizontal space.
 
-Researched answer:
+Researched answer: Mostly got it, though I wasn't aware what exactly 'inline' styling meant before. great for highlighting stuff!
 
 ## Looking Ahead: Terms for Next Week
 
-1. Object-oriented programming:
+1. Object-oriented programming: taking the stuff we've been doing with Javascript objects and applying that logic to as many things as possible while programming.
+***...But this one goes to 11...***
 
-2. Ruby:
+2. Ruby: high level user-friendly (abstracted AF so it's legible by humans) programming language invented by Yukihiro "Matz" Matsumoto in the 90's during his hipster phase, because C-based languages weren't 'elegant' enough for him. 
+***I jest, of course. 90's japan is an interesting period of history, and why/how anyone would create a new programming language in the middle of a huge recession must be a fascinating story.***
 
-3. Implicit return:
+3. Implicit return: Does what it says on the tin. lets you skip writing explicit 'return' statements in languages that support it. Like Ruby.
 
-4. Ruby blocks:
+4. Ruby blocks: Kinda like functions in other languages. They are the chunks of code that actually 'do' stuff.
+***countdown to the Stephen Universe jokes starts now.***
 
-5. Ruby hashes:
+5. Ruby hashes: Those are what we call the key-value pair containers in Ruby. like the objects we know in Javascript but a bit more versatile since any data type can be used as the key.
